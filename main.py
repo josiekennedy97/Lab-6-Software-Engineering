be9af8c008f18f3ee3fa37a5d76430e690a923b5
josiekennedy97 <josiekennedy@ufl.edu>
2023-07-18 22:21:54 -0400

    this is my encoded function and main function

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-#this is my first github edition
+def password_encoder(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
 
-print("hello world")
+while True:
+    print("Menu\n-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option = input("Please enter an option: ")
+
+    if option == "1":
+        password = input("Please enter your password to encode: ")
+        encoded_password = password_encoder(password)
+        print("Your password has been encoded and stored!")
+
+    elif option == "2":
+        # Implement password decoding logic here
+        pass
+
+    elif option == "3":
+        break
+
+    else:
+        print("Invalid option. Please try again.\n")
+
